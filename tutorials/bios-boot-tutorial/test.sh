15e249ec1bd1f19d6dc39ac02d764029279d112b
Jonathan Glanz <jonathan.glanz@gmail.com>
2025-09-02 08:52:56 -0400

    Integrating CDT & LLVM reference into `Dockerfile`

--- tutorials/bios-boot-tutorial/test.sh
+++ tutorials/bios-boot-tutorial/test.sh
@@ -1,1 +1,11 @@
-python3 bios-boot-tutorial.py --clio=clio --nodeop=nodeop --kiod=kiod --contracts-dir="~/system-contracts-3.1/build/contracts" -w -a --user-limit=10 --producer-limit=3
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Determine script paths
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BASE_DIR=$(realpath "${SCRIPT_DIR}/../..")
+WIRE_ORG_DIR=$(realpath "${SCRIPT_DIR}/../../..")
+WIRE_SYS_CONTRACT_DIR=$(realpath "${WIRE_ORG_DIR}/wire-system-contracts")
+
+python3 bios-boot-tutorial.py --clio=clio --nodeop=nodeop --kiod=kiod --contracts-dir="${WIRE_SYS_CONTRACT_DIR}/build/contracts" -w -a --user-limit=10 --producer-limit=3
