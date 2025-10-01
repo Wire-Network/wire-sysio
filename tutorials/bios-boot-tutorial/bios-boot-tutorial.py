1bb360f693eae2bea2f132b768db0898232682e0
kevin Heifner <kevin@wire.network>
2025-10-01 12:16:31 -0500

    WIP: Steps toward getting bios-boot-tutorial.py working. We may decide to just remove this instead of getting it to work.

--- tutorials/bios-boot-tutorial/bios-boot-tutorial.py
+++ tutorials/bios-boot-tutorial/bios-boot-tutorial.py
@@ -20,13 +20,10 @@ systemAccounts = [
     'sysio.bpay',
     'sysio.msig',
     'sysio.names',
-    'sysio.ram',
-    'sysio.ramfee',
     'sysio.saving',
-    'sysio.stake',
     'sysio.token',
     'sysio.vpay',
-    'sysio.rex',
+    'sysio.acct'
 ]
 
 def jsonArg(a):
@@ -160,30 +157,14 @@ def allocateFunds(b, e):
         accounts[i]['funds'] = funds
     return total
 
-def createStakedAccounts(b, e):
-    ramFunds = round(args.ram_funds * 10000)
-    configuredMinStake = round(args.min_stake * 10000)
-    maxUnstaked = round(args.max_unstaked * 10000)
+def createAccounts(b, e):
     for i in range(b, e):
         a = accounts[i]
-        funds = a['funds']
         print('#' * 80)
-        print('# %d/%d %s %s' % (i, e, a['name'], intToCurrency(funds)))
+        print('# %d/%d %s' % (i, e, a['name']))
         print('#' * 80)
-        if funds < ramFunds:
-            print('skipping %s: not enough funds to cover ram' % a['name'])
-            continue
-        minStake = min(funds - ramFunds, configuredMinStake)
-        unstaked = min(funds - ramFunds - minStake, maxUnstaked)
-        stake = funds - ramFunds - unstaked
-        stakeNet = round(stake / 2)
-        stakeCpu = stake - stakeNet
-        print('%s: total funds=%s, ram=%s, net=%s, cpu=%s, unstaked=%s' % (a['name'], intToCurrency(a['funds']), intToCurrency(ramFunds), intToCurrency(stakeNet), intToCurrency(stakeCpu), intToCurrency(unstaked)))
-        assert(funds == ramFunds + stakeNet + stakeCpu + unstaked)
-        retry(args.clio + 'system newaccount --transfer sysio %s %s --stake-net "%s" --stake-cpu "%s" --buy-ram "%s"   ' %
-            (a['name'], a['pub'], intToCurrency(stakeNet), intToCurrency(stakeCpu), intToCurrency(ramFunds)))
-        if unstaked:
-            retry(args.clio + 'transfer sysio %s "%s"' % (a['name'], intToCurrency(unstaked)))
+        retry(args.clio + 'system newaccount sysio %s %s ' %
+            (a['name'], a['pub']))
 
 def regProducers(b, e):
     for i in range(b, e):
@@ -193,16 +174,6 @@ def regProducers(b, e):
 def listProducers():
     run(args.clio + 'system listproducers')
 
-def vote(b, e):
-    for i in range(b, e):
-        voter = accounts[i]['name']
-        k = args.num_producers_vote
-        if k > numProducers:
-            k = numProducers - 1
-        prods = random.sample(range(firstProducer, firstProducer + numProducers), k)
-        prods = ' '.join(map(lambda x: accounts[x]['name'], prods))
-        retry(args.clio + 'system voteproducer prods ' + voter + ' ' + prods)
-
 def claimRewards():
     table = getJsonOutput(args.clio + 'get table sysio sysio producers -l 100')
     times = []
@@ -210,15 +181,6 @@ def claimRewards():
         if row['unpaid_blocks'] and not row['last_claim_time']:
             times.append(getJsonOutput(args.clio + 'system claimrewards -j ' + row['owner'])['processed']['elapsed'])
     print('Elapsed time for claimrewards:', times)
-
-def proxyVotes(b, e):
-    vote(firstProducer, firstProducer + 1)
-    proxy = accounts[firstProducer]['name']
-    retry(args.clio + 'system regproxy ' + proxy)
-    sleep(1.0)
-    for i in range(b, e):
-        voter = accounts[i]['name']
-        retry(args.clio + 'system voteproducer proxy ' + voter + ' ' + proxy)
 
 def updateAuth(account, permission, parent, controller):
     run(args.clio + 'push action sysio updateauth' + jsonArg({
@@ -267,14 +229,6 @@ def msigApproveReplaceSystem(proposer, proposalName):
 def msigExecReplaceSystem(proposer, proposalName):
     retry(args.clio + 'multisig exec ' + proposer + ' ' + proposalName + ' -p ' + proposer)
 
-def msigReplaceSystem():
-    run(args.clio + 'push action sysio buyrambytes' + jsonArg(['sysio', accounts[0]['name'], 200000]) + '-p sysio')
-    sleep(1)
-    msigProposeReplaceSystem(accounts[0]['name'], 'fast.unstake')
-    sleep(1)
-    msigApproveReplaceSystem(accounts[0]['name'], 'fast.unstake')
-    msigExecReplaceSystem(accounts[0]['name'], 'fast.unstake')
-
 def produceNewAccounts():
     with open('newusers', 'w') as f:
         for i in range(120_000, 200_000):
@@ -296,6 +250,7 @@ def stepInstallSystemContracts():
     run(args.clio + 'set contract sysio.token ' + args.contracts_dir + '/sysio.token/')
     run(args.clio + 'set contract sysio.msig ' + args.contracts_dir + '/sysio.msig/')
 def stepCreateTokens():
+    run(args.clio + 'push action sysio setpriv' + jsonArg(['sysio.token', 1]) + '-p sysio@active')
     run(args.clio + 'push action sysio.token create \'["sysio", "10000000000.0000 %s"]\' -p sysio.token' % (args.symbol))
     totalAllocation = allocateFunds(0, len(accounts))
     run(args.clio + 'push action sysio.token issue \'["sysio", "%s", "memo"]\' -p sysio' % intToCurrency(totalAllocation))
@@ -315,57 +270,12 @@ def stepSetSystemContract():
     # action that allows activating desired protocol features prior to 
     # deploying a system contract with more features such as sysio.bios
     # or sysio.system
-    retry(args.clio + 'set contract sysio ' + args.contracts_dir + '/sysio.boot/')
+    retry(args.clio + 'set contract sysio ' + args.bios_contract_dir + '/sysio.bios/')
     sleep(3)
 
     # activate remaining features
-    # ACTION_RETURN_VALUE
-    retry(args.clio + 'push action sysio activate \'["c3a6138c5061cf291310887c0b5c71fcaffeab90d5deb50d3b9e687cead45071"]\' -p sysio@active')
-    # CONFIGURABLE_WASM_LIMITS2
-    retry(args.clio + 'push action sysio activate \'["d528b9f6e9693f45ed277af93474fd473ce7d831dae2180cca35d907bd10cb40"]\' -p sysio@active')
-    # BLOCKCHAIN_PARAMETERS
-    retry(args.clio + 'push action sysio activate \'["5443fcf88330c586bc0e5f3dee10e7f63c76c00249c87fe4fbf7f38c082006b4"]\' -p sysio@active')
-    # GET_SENDER
-    retry(args.clio + 'push action sysio activate \'["f0af56d2c5a48d60a4a5b5c903edfb7db3a736a94ed589d0b797df33ff9d3e1d"]\' -p sysio@active')
-    # FORWARD_SETCODE
-    retry(args.clio + 'push action sysio activate \'["2652f5f96006294109b3dd0bbde63693f55324af452b799ee137a81a905eed25"]\' -p sysio@active')
-    # ONLY_BILL_FIRST_AUTHORIZER
-    retry(args.clio + 'push action sysio activate \'["8ba52fe7a3956c5cd3a656a3174b931d3bb2abb45578befc59f283ecd816a405"]\' -p sysio@active')
-    # RESTRICT_ACTION_TO_SELF
-    retry(args.clio + 'push action sysio activate \'["ad9e3d8f650687709fd68f4b90b41f7d825a365b02c23a636cef88ac2ac00c43"]\' -p sysio@active')
-    # DISALLOW_EMPTY_PRODUCER_SCHEDULE
-    retry(args.clio + 'push action sysio activate \'["68dcaa34c0517d19666e6b33add67351d8c5f69e999ca1e37931bc410a297428"]\' -p sysio@active')
-    # FIX_LINKAUTH_RESTRICTION
-    retry(args.clio + 'push action sysio activate \'["e0fb64b1085cc5538970158d05a009c24e276fb94e1a0bf6a528b48fbc4ff526"]\' -p sysio@active')
-    # REPLACE_DEFERRED
-    retry(args.clio + 'push action sysio activate \'["ef43112c6543b88db2283a2e077278c315ae2c84719a8b25f25cc88565fbea99"]\' -p sysio@active')
-    # NO_DUPLICATE_DEFERRED_ID
-    retry(args.clio + 'push action sysio activate \'["4a90c00d55454dc5b059055ca213579c6ea856967712a56017487886a4d4cc0f"]\' -p sysio@active')
-    # ONLY_LINK_TO_EXISTING_PERMISSION
-    retry(args.clio + 'push action sysio activate \'["1a99a59d87e06e09ec5b028a9cbb7749b4a5ad8819004365d02dc4379a8b7241"]\' -p sysio@active')
-    # RAM_RESTRICTIONS
-    retry(args.clio + 'push action sysio activate \'["4e7bf348da00a945489b2a681749eb56f5de00b900014e137ddae39f48f69d67"]\' -p sysio@active')
-    # WEBAUTHN_KEY
-    retry(args.clio + 'push action sysio activate \'["4fca8bd82bbd181e714e283f83e1b45d95ca5af40fb89ad3977b653c448f78c2"]\' -p sysio@active')
-    # WTMSIG_BLOCK_SIGNATURES
-    retry(args.clio + 'push action sysio activate \'["299dcb6af692324b899b39f16d5a530a33062804e41f09dc97e9f156b4476707"]\' -p sysio@active')
-    # GET_CODE_HASH
-    retry(args.clio + 'push action sysio activate \'["bcd2a26394b36614fd4894241d3c451ab0f6fd110958c3423073621a70826e99"]\' -p sysio@active')
-    # GET_BLOCK_NUM
-    retry(args.clio + 'push action sysio activate \'["35c2186cc36f7bb4aeaf4487b36e57039ccf45a9136aa856a5d569ecca55ef2b"]\' -p sysio@active')
-    # CRYPTO_PRIMITIVES
-    retry(args.clio + 'push action sysio activate \'["6bcb40a24e49c26d0a60513b6aeb8551d264e4717f306b81a37a5afb3b47cedc"]\' -p sysio@active')
-    # BLS_PRIMITIVES2
-    retry(args.clio + 'push action sysio activate \'["7ab0d893e39c01d365ad7f66a2cb8fb02179135c5a0cf16c40645d972e47911d"]\' -p sysio@active')
-    # DISABLE_DEFERRED_TRXS_STAGE_1 - DISALLOW NEW DEFERRED TRANSACTIONS
-    retry(args.clio + 'push action sysio activate \'["2ce18707fa426ea351704ded644b679a87188967b1098cff60ab4a3f35da106e"]\' -p sysio@active')
-    # DISABLE_DEFERRED_TRXS_STAGE_2 - PREVENT PREVIOUSLY SCHEDULED DEFERRED TRANSACTIONS FROM REACHING OTHER NODE
-    # THIS DEPENDS ON DISABLE_DEFERRED_TRXS_STAGE_1
-    retry(args.clio + 'push action sysio activate \'["6cefed65f1f6a04fc82e949b06c0df0e9f5370855353cd3b543e4b5d4ff3dabf"]\' -p sysio@active')
-    # DISABLE_COMPRESSION_IN_TRANSACTION_MERKLE
-    retry(args.clio + 'push action sysio activate \'["71d53c85a760da4eaa6934b5a94eb93426713d2ff74d8fa598e245faa469e573"]\' -p sysio@active')
-    # MULTIPLE_STATE_ROOTS_SUPPORTED
-    retry(args.clio + 'push action sysio activate \'["4d2304c58a30f8ee9d5cf2ac9ac04c3c67ae0cd113be15b746de1a1db07d3b18"]\' -p sysio@active')
+    # RESERVED_FIRST_PROTOCOL_FEATURE
+    retry(args.clio + 'push action sysio activate \'["30df9517cb8808f198723c030f597bb64645d52315f2e8f1ea77424ea33d896a"]\' -p sysio@active')
     sleep(1)
 
     # install sysio.system latest version
@@ -377,8 +287,8 @@ def stepSetSystemContract():
 def stepInitSystemContract():
     run(args.clio + 'push action sysio init' + jsonArg(['0', '4,' + args.symbol]) + '-p sysio@active')
     sleep(1)
-def stepCreateStakedAccounts():
-    createStakedAccounts(0, len(accounts))
+def stepCreateAccounts():
+    createAccounts(0, len(accounts))
 def stepRegProducers():
     regProducers(firstProducer, firstProducer + numProducers)
     sleep(1)
@@ -386,13 +296,6 @@ def stepRegProducers():
 def stepStartProducers():
     startProducers(firstProducer, firstProducer + numProducers)
     sleep(args.producer_sync_delay)
-def stepVote():
-    vote(0, 0 + args.num_voters)
-    sleep(1)
-    listProducers()
-    sleep(5)
-def stepProxyVotes():
-    proxyVotes(0, 0 + args.num_voters)
 def stepResign():
     resign('sysio', 'sysio.prods')
     for a in systemAccounts:
@@ -412,17 +315,14 @@ commands = [
     ('b', 'boot',               stepStartBoot,              True,    "Start boot node"),
     ('s', 'sys',                createSystemAccounts,       True,    "Create system accounts (sysio.*)"),
     ('c', 'contracts',          stepInstallSystemContracts, True,    "Install system contracts (token, msig)"),
-    ('t', 'tokens',             stepCreateTokens,           True,    "Create tokens"),
     ('S', 'sys-contract',       stepSetSystemContract,      True,    "Set system contract"),
+    ('t', 'tokens',             stepCreateTokens,           True,    "Create tokens"),
     ('I', 'init-sys-contract',  stepInitSystemContract,     True,    "Initialiaze system contract"),
-    ('T', 'stake',              stepCreateStakedAccounts,   True,    "Create staked accounts"),
+    ('T', 'stake',              stepCreateAccounts,         True,    "Create accounts"),
     ('p', 'reg-prod',           stepRegProducers,           True,    "Register producers"),
     ('P', 'start-prod',         stepStartProducers,         True,    "Start producers"),
-    ('v', 'vote',               stepVote,                   True,    "Vote for producers"),
     ('R', 'claim',              claimRewards,               True,    "Claim rewards"),
-    ('x', 'proxy',              stepProxyVotes,             True,    "Proxy votes"),
     ('q', 'resign',             stepResign,                 True,    "Resign sysio"),
-    ('m', 'msg-replace',        msigReplaceSystem,          False,   "Replace system contract using msig"),
     ('X', 'xfer',               stepTransfer,               False,   "Random transfer tokens (infinite loop)"),
     ('l', 'log',                stepLog,                    True,    "Show tail of node's log"),
 ]
@@ -433,7 +333,7 @@ parser.add_argument('--clio', metavar='', help="Clio command", default='../../bu
 parser.add_argument('--nodeop', metavar='', help="Path to nodeop binary", default='../../build/programs/nodeop/nodeop')
 parser.add_argument('--kiod', metavar='', help="Path to kiod binary", default='../../build/programs/kiod/kiod')
 parser.add_argument('--contracts-dir', metavar='', help="Path to latest contracts directory", default='../../build/contracts/')
-parser.add_argument('--old-contracts-dir', metavar='', help="Path to 1.8.x contracts directory", default='../../build/contracts/')
+parser.add_argument('--bios-contract-dir', metavar='', help="Path to sysio.bios contract directory", default='../../build/contracts/')
 parser.add_argument('--nodes-dir', metavar='', help="Path to nodes directory", default='./nodes/')
 parser.add_argument('--genesis', metavar='', help="Path to genesis.json", default="./genesis.json")
 parser.add_argument('--wallet-dir', metavar='', help="Path to wallet directory", default='./wallet/')
@@ -441,13 +341,8 @@ parser.add_argument('--log-path', metavar='', help="Path to log file", default='
 parser.add_argument('--symbol', metavar='', help="The sysio.system symbol", default='SYS')
 parser.add_argument('--user-limit', metavar='', help="Max number of users. (0 = no limit)", type=int, default=3000)
 parser.add_argument('--max-user-keys', metavar='', help="Maximum user keys to import into wallet", type=int, default=10)
-parser.add_argument('--ram-funds', metavar='', help="How much funds for each user to spend on ram", type=float, default=0.1)
-parser.add_argument('--min-stake', metavar='', help="Minimum stake before allocating unstaked funds", type=float, default=0.9)
-parser.add_argument('--max-unstaked', metavar='', help="Maximum unstaked funds", type=float, default=10)
 parser.add_argument('--producer-limit', metavar='', help="Maximum number of producers. (0 = no limit)", type=int, default=0)
 parser.add_argument('--min-producer-funds', metavar='', help="Minimum producer funds", type=float, default=1000.0000)
-parser.add_argument('--num-producers-vote', metavar='', help="Number of producers for which each user votes", type=int, default=20)
-parser.add_argument('--num-voters', metavar='', help="Number of voters", type=int, default=10)
 parser.add_argument('--num-senders', metavar='', help="Number of users to transfer funds randomly", type=int, default=10)
 parser.add_argument('--producer-sync-delay', metavar='', help="Time (s) to sleep to allow producers to sync", type=int, default=80)
 parser.add_argument('-a', '--all', action='store_true', help="Do everything marked with (*)")
@@ -465,7 +360,7 @@ for (flag, command, function, inAll, help) in commands:
 args = parser.parse_args()
 
 # Leave a space in front of --url in case the user types clio alone
-args.clio += ' --url http://0.0.0.0:%d ' % args.http_port
+args.clio += ' --url http://127.0.0.1:%d ' % args.http_port
 
 logFile = open(args.log_path, 'a')
 
