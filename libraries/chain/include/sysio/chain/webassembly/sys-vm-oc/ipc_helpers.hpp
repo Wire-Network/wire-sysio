AntelopeIO/leap

use glibc's memfd_create()

libraries/chain/include/eosio/chain/webassembly/eos-vm-oc/ipc_helpers.hpp
@@ -6,7 +6,6 @@
 
 #include <vector>
 
-#include <sys/syscall.h>
 #include <linux/memfd.h>
 
 namespace eosio { namespace chain { namespace eosvmoc {
@@ -54,7 +53,7 @@ bool write_message_with_fds(int fd_to_send_to, const eosvmoc_message& message, c
 
 template<typename T>
 wrapped_fd memfd_for_bytearray(const T& bytes) {
-   int fd = syscall(SYS_memfd_create, "eosvmoc_code", MFD_CLOEXEC);
+   int fd = memfd_create("eosvmoc_code", MFD_CLOEXEC);
    FC_ASSERT(fd >= 0, "Failed to create memfd");
    FC_ASSERT(ftruncate(fd, bytes.size()) == 0, "failed to grow memfd");
    if(bytes.size()) {

libraries/chain/webassembly/runtimes/eos-vm-oc/memory.cpp
@@ -5,7 +5,6 @@
 #include <fc/scoped_exit.hpp>
 
 #include <unistd.h>
-#include <sys/syscall.h>
 #include <sys/mman.h>
 #include <linux/memfd.h>
 
@@ -14,7 +13,7 @@ namespace eosio { namespace chain { namespace eosvmoc {
 memory::memory(uint64_t sliced_pages) {
    uint64_t number_slices = sliced_pages + 1;
    uint64_t wasm_memory_size = sliced_pages * wasm_constraints::wasm_page_size;
-   int fd = syscall(SYS_memfd_create, "eosvmoc_mem", MFD_CLOEXEC);
+   int fd = memfd_create("eosvmoc_mem", MFD_CLOEXEC);
    FC_ASSERT(fd >= 0, "Failed to create memory memfd");
    auto cleanup_fd = fc::make_scoped_exit([&fd](){close(fd);});
    int ret = ftruncate(fd, wasm_memory_size+memory_prologue_size);

Wire-Network/wire-sysio

libraries/chain/include/sysio/chain/webassembly/sys-vm-oc/ipc_helpers.hpp
@@ -6,7 +6,6 @@
 
 #include <vector>
 
-#include <sys/syscall.h>
 #include <linux/memfd.h>
 
 namespace sysio { namespace chain { namespace sysvmoc {
@@ -54,7 +53,7 @@ bool write_message_with_fds(int fd_to_send_to, const sysvmoc_message& message, c
 
 template<typename T>
 wrapped_fd memfd_for_bytearray(const T& bytes) {
-   int fd = syscall(SYS_memfd_create, "sysvmoc_code", MFD_CLOEXEC);
+   int fd = memfd_create("sysvmoc_code", MFD_CLOEXEC);
    FC_ASSERT(fd >= 0, "Failed to create memfd");
    FC_ASSERT(ftruncate(fd, bytes.size()) == 0, "failed to grow memfd");
    if(bytes.size()) {

libraries/chain/webassembly/runtimes/sys-vm-oc/memory.cpp
@@ -5,7 +5,6 @@
 #include <fc/scoped_exit.hpp>
 
 #include <unistd.h>
-#include <sys/syscall.h>
 #include <sys/mman.h>
 #include <linux/memfd.h>
 
@@ -14,7 +13,7 @@ namespace sysio { namespace chain { namespace sysvmoc {
 memory::memory(uint64_t sliced_pages) {
    uint64_t number_slices = sliced_pages + 1;
    uint64_t wasm_memory_size = sliced_pages * wasm_constraints::wasm_page_size;
-   int fd = syscall(SYS_memfd_create, "sysvmoc_mem", MFD_CLOEXEC);
+   int fd = memfd_create("sysvmoc_mem", MFD_CLOEXEC);
    FC_ASSERT(fd >= 0, "Failed to create memory memfd");
    auto cleanup_fd = fc::make_scoped_exit([&fd](){close(fd);});
    int ret = ftruncate(fd, wasm_memory_size+memory_prologue_size);