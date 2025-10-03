AntelopeIO/leap

rename block_header_state.{hpp,cpp} to block_header_state_legacy.{hpp, cpp} and rename block_state.{hpp,cpp} to block_state_legacy.{hpp,cpp}

libraries/chain/deep_mind.cpp
@@ -1,5 +1,5 @@
 #include <eosio/chain/deep_mind.hpp>
-#include <eosio/chain/block_state.hpp>
+#include <eosio/chain/block_state_legacy.hpp>
 #include <eosio/chain/generated_transaction_object.hpp>
 #include <eosio/chain/contract_table_objects.hpp>
 #include <eosio/chain/resource_limits_private.hpp>

libraries/chain/include/eosio/chain/snapshot_scheduler.hpp
@@ -2,7 +2,7 @@
 
 #include <eosio/chain/pending_snapshot.hpp>
 
-#include <eosio/chain/block_state.hpp>
+#include <eosio/chain/block_state_legacy.hpp>
 #include <eosio/chain/config.hpp>
 #include <eosio/chain/exceptions.hpp>
 #include <eosio/chain/resource_limits.hpp>

plugins/chain_interface/include/eosio/chain/plugin_interface.hpp
@@ -4,7 +4,7 @@
 #include <appbase/method.hpp>
 
 #include <eosio/chain/block.hpp>
-#include <eosio/chain/block_state.hpp>
+#include <eosio/chain/block_state_legacy.hpp>
 #include <eosio/chain/transaction_metadata.hpp>
 #include <eosio/chain/trace.hpp>
 

plugins/chain_plugin/include/eosio/chain_plugin/account_query_db.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <eosio/chain/types.hpp>
-#include <eosio/chain/block_state.hpp>
+#include <eosio/chain/block_state_legacy.hpp>
 #include <eosio/chain/trace.hpp>
 
 namespace eosio::chain_apis {

plugins/trace_api_plugin/test/test_extraction.cpp
@@ -5,7 +5,7 @@
 #include <eosio/chain/trace.hpp>
 #include <eosio/chain/transaction.hpp>
 #include <eosio/chain/block.hpp>
-#include <eosio/chain/block_state.hpp>
+#include <eosio/chain/block_state_legacy.hpp>
 
 #include <eosio/trace_api/test_common.hpp>
 #include <eosio/trace_api/chain_extraction.hpp>

Wire-Network/wire-sysio

libraries/chain/deep_mind.cpp
@@ -1,5 +1,5 @@
 #include <sysio/chain/deep_mind.hpp>
-#include <sysio/chain/block_state.hpp>
+#include <sysio/chain/block_state_legacy.hpp>
 #include <sysio/chain/contract_table_objects.hpp>
 #include <sysio/chain/resource_limits_private.hpp>
 #include <sysio/chain/permission_object.hpp>

libraries/chain/include/sysio/chain/snapshot_scheduler.hpp
@@ -2,7 +2,7 @@
 
 #include <sysio/chain/pending_snapshot.hpp>
 
-#include <sysio/chain/block_state.hpp>
+#include <sysio/chain/block_state_legacy.hpp>
 #include <sysio/chain/config.hpp>
 #include <sysio/chain/exceptions.hpp>
 #include <sysio/chain/resource_limits.hpp>

plugins/chain_interface/include/sysio/chain/plugin_interface.hpp
@@ -4,7 +4,7 @@
 #include <appbase/method.hpp>
 
 #include <sysio/chain/block.hpp>
-#include <sysio/chain/block_state.hpp>
+#include <sysio/chain/block_state_legacy.hpp>
 #include <sysio/chain/transaction_metadata.hpp>
 #include <sysio/chain/trace.hpp>
 

plugins/chain_plugin/include/sysio/chain_plugin/account_query_db.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <sysio/chain/types.hpp>
-#include <sysio/chain/block_state.hpp>
+#include <sysio/chain/block_state_legacy.hpp>
 #include <sysio/chain/trace.hpp>
 
 namespace sysio::chain_apis {

plugins/trace_api_plugin/test/test_extraction.cpp
@@ -5,7 +5,7 @@
 #include <sysio/chain/trace.hpp>
 #include <sysio/chain/transaction.hpp>
 #include <sysio/chain/block.hpp>
-#include <sysio/chain/block_state.hpp>
+#include <sysio/chain/block_state_legacy.hpp>
 
 #include <sysio/trace_api/test_common.hpp>
 #include <sysio/trace_api/chain_extraction.hpp>