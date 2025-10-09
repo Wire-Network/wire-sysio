98120d4ca823fa36e79462d44752837aaa0be93b
Jonathan Glanz <jonathan.glanz@gmail.com>
2025-10-09 17:12:18 -0400

    Added tooling to completely generate an AIO (All-In-One) configuration, including unique `chain_id` & keys.
    > NOTE: Currently, the only thing missing is auto import to the user's (`SYS`) wallet,
    > which will be added tomorrow.
    
    ## Example command line to generate working genesis & config
    
    ```sh
    sys-util chain-configure \
                --target=/opt/wire-network/chain-001 \
                --template=aio \
                --overwrite
    ```
    
    Add configuration files for chain setup and update plugin structure
    
    - Introduced `genesis.template.json` for genesis configuration.
    - Added `config.template.ini` for application configuration.
    - Created `sys-util (chain-configure)` run configuration XML.
    - Renamed `config.ini` to `config.template.ini` for clarity.
    - Updated CMakeLists.txt to include new configuration files and adjust plugin dependencies.

--- libraries/plugin_common/include/sysio/plugin/common/abstract_operator_plugin.hpp
+++ libraries/plugin_common/include/sysio/plugin/common/abstract_operator_plugin.hpp
@@ -9,10 +9,10 @@ namespace sysio::plugin {
 // It remains abstract by declaring a pure virtual marker method, which derived operator plugins should implement.
 
 template <typename Impl>
-class operator_plugin_base : public appbase::plugin<Impl> {
+class abstract_operator_plugin : public appbase::plugin<Impl> {
 public:
    using appbase::plugin<Impl>::plugin; // inherit constructors
-   virtual ~operator_plugin_base() = default;
+   virtual ~abstract_operator_plugin() = default;
 
    // Marker method to keep this base abstract. Implement in derived operator plugins if needed.
    virtual const char* operator_kind() const = 0;
