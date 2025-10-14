AntelopeIO/leap

specialized to_variant() for chainbase::environment's packedness

plugins/chain_plugin/chain_plugin.cpp
@@ -16,10 +16,9 @@
 #include <eosio/chain_plugin/trx_finality_status_processing.hpp>
 #include <eosio/chain/permission_link_object.hpp>
 #include <eosio/chain/global_property_object.hpp>
+#include <eosio/chain/chainbase_environment.hpp>
 
 #include <eosio/resource_monitor_plugin/resource_monitor_plugin.hpp>
-
-#include <chainbase/environment.hpp>
 
 #include <boost/signals2/connection.hpp>
 #include <boost/algorithm/string.hpp>
@@ -28,13 +27,6 @@
 #include <fc/io/json.hpp>
 #include <fc/variant.hpp>
 #include <cstdlib>
-
-// reflect chainbase::environment for --print-build-info option
-FC_REFLECT_ENUM( chainbase::environment::os_t,
-                 (OS_LINUX)(OS_MACOS)(OS_WINDOWS)(OS_OTHER) )
-FC_REFLECT_ENUM( chainbase::environment::arch_t,
-                 (ARCH_X86_64)(ARCH_ARM)(ARCH_RISCV)(ARCH_OTHER) )
-FC_REFLECT(chainbase::environment, (debug)(os)(arch)(boost_version)(compiler) )
 
 const std::string deep_mind_logger_name("deep-mind");
 eosio::chain::deep_mind_handler _deep_mind_log;

programs/leap-util/actions/chain.cpp
@@ -10,7 +10,7 @@
 
 #include <eosio/chain/block_log.hpp>
 #include <eosio/chain/exceptions.hpp>
-#include <chainbase/environment.hpp>
+#include <eosio/chain/chainbase_environment.hpp>
 
 #include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
@@ -18,15 +18,6 @@
 
 using namespace eosio;
 using namespace eosio::chain;
-
-
-// reflect chainbase::environment for --print-build-info option
-FC_REFLECT_ENUM(chainbase::environment::os_t,
-                (OS_LINUX) (OS_MACOS) (OS_WINDOWS) (OS_OTHER))
-FC_REFLECT_ENUM(chainbase::environment::arch_t,
-                (ARCH_X86_64) (ARCH_ARM) (ARCH_RISCV) (ARCH_OTHER))
-FC_REFLECT(chainbase::environment, (debug) (os) (arch) (boost_version) (compiler))
-
 
 void chain_actions::setup(CLI::App& app) {
    auto* sub = app.add_subcommand("chain-state", "chain utility");

Wire-Network/wire-sysio

plugins/chain_plugin/chain_plugin.cpp
@@ -17,10 +17,9 @@
 #include <sysio/chain/permission_link_object.hpp>
 #include <sysio/chain/subjective_billing.hpp>
 #include <sysio/chain/global_property_object.hpp>
+#include <sysio/chain/chainbase_environment.hpp>
 
 #include <sysio/resource_monitor_plugin/resource_monitor_plugin.hpp>
-
-#include <chainbase/environment.hpp>
 
 #include <boost/signals2/connection.hpp>
 #include <boost/algorithm/string.hpp>
@@ -29,13 +28,6 @@
 #include <fc/io/json.hpp>
 #include <fc/variant.hpp>
 #include <cstdlib>
-
-// reflect chainbase::environment for --print-build-info option
-FC_REFLECT_ENUM( chainbase::environment::os_t,
-                 (OS_LINUX)(OS_MACOS)(OS_WINDOWS)(OS_OTHER) )
-FC_REFLECT_ENUM( chainbase::environment::arch_t,
-                 (ARCH_X86_64)(ARCH_ARM)(ARCH_RISCV)(ARCH_OTHER) )
-FC_REFLECT(chainbase::environment, (debug)(os)(arch)(boost_version)(compiler) )
 
 const std::string deep_mind_logger_name("deep-mind");
 sysio::chain::deep_mind_handler _deep_mind_log;

programs/sys-util/actions/chain.cpp
@@ -10,7 +10,7 @@
 
 #include <sysio/chain/block_log.hpp>
 #include <sysio/chain/exceptions.hpp>
-#include <chainbase/environment.hpp>
+#include <sysio/chain/chainbase_environment.hpp>
 
 #include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
@@ -18,15 +18,6 @@
 
 using namespace sysio;
 using namespace sysio::chain;
-
-
-// reflect chainbase::environment for --print-build-info option
-FC_REFLECT_ENUM(chainbase::environment::os_t,
-                (OS_LINUX) (OS_MACOS) (OS_WINDOWS) (OS_OTHER))
-FC_REFLECT_ENUM(chainbase::environment::arch_t,
-                (ARCH_X86_64) (ARCH_ARM) (ARCH_RISCV) (ARCH_OTHER))
-FC_REFLECT(chainbase::environment, (debug) (os) (arch) (boost_version) (compiler))
-
 
 void chain_actions::setup(CLI::App& app) {
    auto* sub = app.add_subcommand("chain-state", "chain utility");