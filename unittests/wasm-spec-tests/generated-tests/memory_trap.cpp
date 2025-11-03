cc5fd2c4f8acf98778dd62ec4bd00180cbb8d557
kevin Heifner <kevin@wire.network>
2025-11-03 15:20:48 -0600

    Split memory_trap_1_check_throw into two diff tests because of long runtime.

--- unittests/wasm-spec-tests/generated-tests/memory_trap.cpp
+++ unittests/wasm-spec-tests/generated-tests/memory_trap.cpp
@@ -41,7 +41,7 @@ BOOST_DATA_TEST_CASE(memory_trap_0_pass, boost::unit_test::data::xrange(10,11), 
 const string wasm_str_memory_trap_1 = base_dir + "/memory_trap.1.wasm";
 std::vector<uint8_t> wasm_memory_trap_1= read_wasm(wasm_str_memory_trap_1.c_str());
 
-BOOST_DATA_TEST_CASE(memory_trap_1_check_throw, boost::unit_test::data::xrange(0,156), index) { try {
+void memory_trap_1_check_throw_common(uint32_t index) {
    TESTER tester;
    tester.produce_block();
    tester.create_account( "wasmtest"_n );
@@ -56,6 +56,14 @@ BOOST_DATA_TEST_CASE(memory_trap_1_check_throw, boost::unit_test::data::xrange(0
 
    BOOST_CHECK_THROW(push_action(tester, std::move(test), "wasmtest"_n.to_uint64_t()), wasm_execution_error);
    tester.produce_block();
+}
+
+BOOST_DATA_TEST_CASE(memory_trap_1_check_throw_1, boost::unit_test::data::xrange(0,78), index) { try {
+   memory_trap_1_check_throw_common(index);
+} FC_LOG_AND_RETHROW() }
+
+BOOST_DATA_TEST_CASE(memory_trap_1_check_throw_2, boost::unit_test::data::xrange(78,156), index) { try {
+   memory_trap_1_check_throw_common(index);
 } FC_LOG_AND_RETHROW() }
 
 BOOST_DATA_TEST_CASE(memory_trap_1_pass, boost::unit_test::data::xrange(156,157), index) { try {
