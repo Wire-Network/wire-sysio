2c9a0bb01c08b858ddf7561e9450ef12eac9fe18
Jonathan Glanz <jonathan.glanz@gmail.com>
2025-12-19 14:54:17 -0500

    Merged svetla's new SysioTester.cmake

--- plugins/outpost_solana_client_plugin/include/sysio/outpost_solana_client_plugin.hpp
+++ plugins/outpost_solana_client_plugin/include/sysio/outpost_solana_client_plugin.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
+
 #include <sysio/outpost_client_plugin.hpp>
 #include <sysio/chain_plugin/chain_plugin.hpp>
-#include <sysio/outpost_client/solana/solana_client.hpp>
+
 #include <sysio/signature_provider_manager_plugin/signature_provider_manager_plugin.hpp>
 
 namespace sysio {
-using namespace sysio::outpost_client::solana;
+
 
 class outpost_solana_client_plugin : public appbase::plugin<outpost_solana_client_plugin> {
 public:
