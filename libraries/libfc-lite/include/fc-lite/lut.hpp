a95328bc5eafd50d6316b9c87146881b23e0e7da
Jonathan Glanz <jonathan.glanz@gmail.com>
2025-12-29 16:37:41 -0500

    Cleaned up `[u]int128`, including variant support

--- libraries/libfc-lite/include/fc-lite/lut.hpp
+++ libraries/libfc-lite/include/fc-lite/lut.hpp
@@ -1,70 +1,70 @@
 #pragma once
 
-#include <string>
 #include <array>
-#include <string_view>
-#include <tuple>
-
 #include <algorithm>
-#include <array>
-#include <functional>
 #include <stdexcept>
-#include <utility>
 #include <utility>
 
 namespace fc {
-template<typename T, typename K>
-using lut_key_getter = std::function<K(T&)>;
 
 template <typename K, typename T, std::size_t N>
 class lut {
    std::array<std::pair<K, T>, N> elements_{};
-   std::array<T,N> values_{};
-   std::array<K,N> keys_{};
 
 public:
    lut()                            = delete;
-   lut(const lut& other)            = delete;
-   lut(lut&& other)                 = delete;
-   lut& operator=(const lut& other) = delete;
-   lut& operator=(lut&& other)      = delete;
+   lut(const lut& other)            = default;
+   lut(lut&& other)                 = default;
+   lut& operator=(const lut& other) = default;
+   lut& operator=(lut&& other)      = default;
 
+   explicit constexpr lut(std::array<std::pair<K, T>, N> elements)
+      : elements_(elements) {}
 
-   constexpr lut(std::initializer_list<std::pair<K, T>> elements) {
-      std::move(elements.begin(), elements.end(), elements_.begin());
-
-      std::transform(elements_.begin(),elements_.end(),values_.begin(), [&] (auto& elem) {return std::get<1>(elem);});
-      std::transform(elements_.begin(),elements_.end(),keys_.begin(), [&] (auto& elem) {return std::get<0>(elem);});
-
-   }
+   template <typename... Args>
+   explicit constexpr lut(Args&&... args)
+      requires(sizeof...(Args) == N) && (std::is_convertible_v<Args, std::pair<K, T>> && ...)
+      : lut(std::array<std::pair<K, T>, N>{std::forward<Args>(args)...}) {}
 
    [[nodiscard]] constexpr std::size_t size() const {
       return N;
    }
 
-   constexpr T lookup(const K &key) const {
-      for (auto &[testKey, value] : elements_) {
-         if (key == testKey) {
-            return value;
+   constexpr const T& lookup(const K &key) const {
+      for (const auto& elem : elements_) {
+         if (key == elem.first) {
+            return elem.second;
          }
       }
-
-      // static_assert(1 != 1, "key not found");
-      // throw std::range_error("key not found");
+      // throw is allowed in constexpr if not reached.
+      throw std::out_of_range("key not found in lut");
    }
 
-   constexpr T operator[](const K &key) const {
+   constexpr const T& operator[](const K &key) const {
       return lookup(key);
    }
 
-   constexpr const std::array<K,N>& keys() const {
-      return keys_;
+   constexpr auto keys() const {
+      std::array<K, N> k{};
+      for (std::size_t i = 0; i < N; ++i) {
+         k[i] = elements_[i].first;
+      }
+      return k;
    }
 
-   constexpr const std::array<T,N>& values() const {
-      return values_;
+   constexpr auto values() const {
+      std::array<T, N> v{};
+      for (std::size_t i = 0; i < N; ++i) {
+         v[i] = elements_[i].second;
+      }
+      return v;
    }
-
-
 };
+
+template <typename K, typename T, typename... Pairs>
+lut(std::pair<K, T>, Pairs...) -> lut<K, T, 1 + sizeof...(Pairs)>;
+
+template <typename K, typename T, std::size_t N>
+lut(std::array<std::pair<K, T>, N>) -> lut<K, T, N>;
+
 } // namespace fc
--- libraries/libfc/include/fc/variant.hpp
+++ libraries/libfc/include/fc/variant.hpp
@@ -766,5 +766,5 @@ namespace fc
 
 #include <fc/reflect/reflect.hpp>
 FC_REFLECT_TYPENAME( fc::variant )
-FC_REFLECT_ENUM( fc::variant::type_id, (null_type)(int64_type)(uint64_type)(int256_type)(uint256_type)(double_type)(bool_type)(string_type)(array_type)(object_type)(blob_type) )
+FC_REFLECT_ENUM( fc::variant::type_id, (null_type)(int64_type)(uint64_type)(int128_type)(uint128_type)(int256_type)(uint256_type)(double_type)(bool_type)(string_type)(array_type)(object_type)(blob_type) )
 FC_REFLECT( fc::blob, (data) );
