dd88e3e0854ee385297df685c52dc33ecc1059aa
kevin Heifner <kevin@wire.network>
2026-02-20 11:59:16 -0600

    Collect all vcpkg dependency licenses into install directory
    This replaces the stale commented-out license paths with a loop that
    collects copyright files from all vcpkg dependencies into
    licenses/sysio/. Also bumps the wire-vcpkg-registry baseline to pick up
    the new vcpkg_install_copyright calls for bls12-381, bn256, and
    boringssl-custom.

--- cmake/licenses.cmake
+++ cmake/licenses.cmake
@@ -1,10 +1,58 @@
-configure_file(${CMAKE_SOURCE_DIR}/LICENSE.md                                                  licenses/sysio/LICENSE.md        COPYONLY)
+# Wire-Sysio license
+configure_file(${CMAKE_SOURCE_DIR}/LICENSE.md licenses/sysio/LICENSE.md COPYONLY)
 
-# TODO: prometheus-cpp, softfloat and CLI11 license files are no longer available since moving to vcpkg
-# The licenses would need to be copied separately from vcpkg or original repositories
-#configure_file(${CMAKE_SOURCE_DIR}/libraries/libfc/libraries/boringssl/boringssl/src/LICENSE   licenses/sysio/LICENSE.boringssl COPYONLY)
-#configure_file(${CMAKE_SOURCE_DIR}/libraries/libfc/include/fc/crypto/webauthn_json/license.txt licenses/sysio/LICENSE.rapidjson COPYONLY)
-# configure_file(${CMAKE_SOURCE_DIR}/libraries/libfc/libraries/bls12-381/LICENSE                 licenses/sysio/LICENSE.bls12-381 COPYONLY)
-#configure_file(${CMAKE_SOURCE_DIR}/libraries/libfc/secp256k1/secp256k1/COPYING                 licenses/sysio/LICENSE.secp256k1 COPYONLY)
-# configure_file(${CMAKE_SOURCE_DIR}/libraries/sys-vm/LICENSE                                    licenses/sysio/LICENSE.sys-vm    COPYONLY)
-configure_file(${CMAKE_SOURCE_DIR}/libraries/wasm-jit/LICENSE                                  licenses/sysio/LICENSE.wavm      COPYONLY)
+# In-tree dependency licenses
+configure_file(${CMAKE_SOURCE_DIR}/libraries/wasm-jit/LICENSE licenses/sysio/LICENSE.wavm COPYONLY)
+
+# vcpkg dependency licenses
+# Collect copyright files from all vcpkg-installed packages into the licenses directory.
+# vcpkg convention: each package places a copyright file in share/<pkg>/copyright.
+set(_vcpkg_share_dir "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/share")
+
+# Packages that use the standard "copyright" filename
+set(_vcpkg_copyright_packages
+   bls12-381
+   bn256
+   boost
+   boringssl-custom
+   bzip2
+   catch2
+   civetweb
+   cli11
+   cryptopp
+   curl
+   ethash
+   fmt
+   gmp
+   gsl-lite
+   gtest
+   liblzma
+   libsodium
+   llvm
+   magic-enum
+   nlohmann-json
+   prometheus-cpp
+   rapidjson
+   secp256k1-internal
+   softfloat
+   spdlog
+   zlib
+   zstd
+)
+
+foreach(_pkg IN LISTS _vcpkg_copyright_packages)
+   set(_src "${_vcpkg_share_dir}/${_pkg}/copyright")
+   if(EXISTS "${_src}")
+      configure_file("${_src}" "licenses/sysio/LICENSE.${_pkg}" COPYONLY)
+   else()
+      message(WARNING "License file not found for vcpkg package: ${_pkg} (expected ${_src})")
+   endif()
+endforeach()
+
+# wire-sys-vm uses LICENSE.md instead of copyright
+set(_sys_vm_license "${_vcpkg_share_dir}/wire-sys-vm/LICENSE.md")
+if(EXISTS "${_sys_vm_license}")
+   configure_file("${_sys_vm_license}" "licenses/sysio/LICENSE.wire-sys-vm" COPYONLY)
+else()
+   message(WARNING "License file not found for vcpkg package: wire-sys-vm (expected ${_sys_vm_license})")
+endif()
