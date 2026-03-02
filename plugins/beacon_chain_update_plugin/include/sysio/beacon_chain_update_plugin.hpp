bbeb9f385b1432dfc6463573ab8a2ea1722e36f5
Brian Johnson <brian.johnson@wire.network>
2026-03-02 11:28:30 -0600

    Initial Crank: Error including cron_plugin

---
+++ plugins/beacon_chain_update_plugin/include/sysio/beacon_chain_update_plugin.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <sysio/outpost_ethereum_client_plugin.hpp>
+
+namespace sysio {
+
+class beacon_chain_update_plugin : public appbase::plugin<beacon_chain_update_plugin> {
+public:
+   APPBASE_PLUGIN_REQUIRES((outpost_ethereum_client_plugin)(signature_provider_manager_plugin))
+   beacon_chain_update_plugin();
+   virtual ~beacon_chain_update_plugin() = default;
+
+   virtual void set_program_options(options_description& cli, options_description& cfg) override;
+
+   virtual void plugin_initialize(const variables_map& options);
+
+   virtual void plugin_startup();
+
+   virtual void plugin_shutdown();
+
+private:
+   std::unique_ptr<class beacon_chain_update_plugin_impl> my;
+};
+
+
+} // namespace sysio
