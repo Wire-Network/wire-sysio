a4c82fea20395a4710cbae0d360e37fc775208ee
Huang-Ming Huang <huangming.huang@gmail.com>
2026-08-07 18:58:06 +0000

    Format Solidity UIC generator test
    Change-Id: I90f0a02a7e967b4f175252033d544d5e8b834cbd

--- libraries/opp/test/uic_cross_generator_solidity/test/UICCrossGenerator.t.sol
+++ libraries/opp/test/uic_cross_generator_solidity/test/UICCrossGenerator.t.sol
@@ -15,30 +15,24 @@ import {
 /// Executes the generated production Solidity codec against the sealed UIC
 /// records owned by wire-sysio's cross-generator compatibility gate.
 contract UICCrossGeneratorTest {
-    bytes private constant EVM_ADDRESS =
-        hex"9965507d1a55bcc2695c58ba16fb37d819b0a4dc";
+    bytes private constant EVM_ADDRESS = hex"9965507d1a55bcc2695c58ba16fb37d819b0a4dc";
     bytes private constant EVM_SIGNATURE =
         hex"0020dca5027647a8282688d535cc783057ece6a950450c63cefafa007ac95688ee2608525965898277231aee8ee079065a8da3cd886a34e2326ae6143b074be787fb";
     bytes private constant EVM_FULL =
         hex"0a0d0a0b756e6465727772697465721218080212149965507d1a55bcc2695c58ba16fb37d819b0a4dc18ac022a420020dca5027647a8282688d535cc783057ece6a950450c63cefafa007ac95688ee2608525965898277231aee8ee079065a8da3cd886a34e2326ae6143b074be787fb30ad0238ae0240af02";
 
-    bytes private constant SVM_ADDRESS =
-        hex"17cb79fb2b4120f2b1ec65e4198d6e08b28e813feb01e4a400839b85e18080ce";
+    bytes private constant SVM_ADDRESS = hex"17cb79fb2b4120f2b1ec65e4198d6e08b28e813feb01e4a400839b85e18080ce";
     bytes private constant SVM_SIGNATURE =
         hex"04962daf636c40b2b7236987387b2d04b2cafb7dd1da8711ae48afb0e3d3baad12311911aecac80f2a05cb4f364a35fe1437c74aeee5b7d51b448ecc758e374091b0a6817ce27ea8df7adc5491f116c0248d7470a819e22d241e5768151371be0e";
     bytes private constant SVM_FULL =
         hex"0a0d0a0b756e64657277726974657212240803122017cb79fb2b4120f2b1ec65e4198d6e08b28e813feb01e4a400839b85e18080ce18ac022a6104962daf636c40b2b7236987387b2d04b2cafb7dd1da8711ae48afb0e3d3baad12311911aecac80f2a05cb4f364a35fe1437c74aeee5b7d51b448ecc758e374091b0a6817ce27ea8df7adc5491f116c0248d7470a819e22d241e5768151371be0e30ad0238ae0240af02";
 
     function testGeneratedSolidityEncoderMatchesSealedEvmVector() public pure {
-        _assertConstructsAndReencodes(
-            ChainKindLib.CHAIN_KIND_EVM, EVM_ADDRESS, EVM_SIGNATURE, EVM_FULL
-        );
+        _assertConstructsAndReencodes(ChainKindLib.CHAIN_KIND_EVM, EVM_ADDRESS, EVM_SIGNATURE, EVM_FULL);
     }
 
     function testGeneratedSolidityEncoderMatchesSealedSvmVector() public pure {
-        _assertConstructsAndReencodes(
-            ChainKindLib.CHAIN_KIND_SVM, SVM_ADDRESS, SVM_SIGNATURE, SVM_FULL
-        );
+        _assertConstructsAndReencodes(ChainKindLib.CHAIN_KIND_SVM, SVM_ADDRESS, SVM_SIGNATURE, SVM_FULL);
     }
 
     function testGeneratedSolidityDecoderRejectsMalformedUint64Varints() public {
@@ -47,9 +41,7 @@ contract UICCrossGeneratorTest {
     }
 
     function decodeAndReencode(bytes memory encoded) external pure returns (bytes memory) {
-        return UnderwriteIntentCommitCodec.encode(
-            UnderwriteIntentCommitCodec.decode(encoded)
-        );
+        return UnderwriteIntentCommitCodec.encode(UnderwriteIntentCommitCodec.decode(encoded));
     }
 
     function _assertConstructsAndReencodes(
@@ -68,18 +60,11 @@ contract UICCrossGeneratorTest {
             reserveCode: 303
         });
         _assertBytesEqual(UnderwriteIntentCommitCodec.encode(uic), expected);
-        _assertBytesEqual(
-            UnderwriteIntentCommitCodec.encode(
-                UnderwriteIntentCommitCodec.decode(expected)
-            ),
-            expected
-        );
+        _assertBytesEqual(UnderwriteIntentCommitCodec.encode(UnderwriteIntentCommitCodec.decode(expected)), expected);
     }
 
     function _assertDecodeReverts(bytes memory malformed) private {
-        (bool success,) = address(this).call(
-            abi.encodeCall(this.decodeAndReencode, (malformed))
-        );
+        (bool success,) = address(this).call(abi.encodeCall(this.decodeAndReencode, (malformed)));
         require(!success, "malformed varint decoded successfully");
     }
 
